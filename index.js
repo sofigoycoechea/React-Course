c2991225a5769a0da29b0b2a8877ceee1ca5d403
Sofía Goycoechea <sofia.goycoechea@kinamic.com>
2025-09-11 20:21:10 -0300

    next

--- index.js
+++ index.js
@@ -48,10 +48,8 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-
-  Person.findByIdAndDelete(id)
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
     .then(result => {
       if (result) {
         response.status(204).end()
@@ -59,13 +57,10 @@ app.delete('/api/persons/:id', (request, response) => {
         response.status(404).end()
       }
     })
-    .catch(error => {
-      console.error(error)
-      response.status(500).end()
-    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
@@ -77,12 +72,12 @@ app.post('/api/persons', (request, response) => {
     number: body.number
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  }).catch(error => response.status(400).json({ error: error.message }))
+  person.save()
+    .then(savedPerson => response.json(savedPerson))
+    .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
   const { number } = request.body
 
   Person.findByIdAndUpdate(
@@ -115,7 +110,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
